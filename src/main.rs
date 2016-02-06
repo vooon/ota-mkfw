44129928bd1de4d7b9a442f85190f6088ca2b194
Vladimir Ermakov <vooon341@gmail.com>
2016-02-06 18:02:06 +0300

    add functions for subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 
 #[macro_use]
 extern crate clap;
-use clap::App;
+use clap::{App, ArgMatches};
 
+extern crate cbor;
+use cbor::Encoder;
+
+
+fn do_mkfw(matches: &ArgMatches) {
+    println!("{:?}", matches);
+}
+
+fn do_upload(matches: &ArgMatches) {
+    println!("{:?}", matches);
+}
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml)
         .version(crate_version!())
         .get_matches();
+
+    match matches.subcommand() {
+        ("mkfw", Some(subm)) => do_mkfw(subm),
+        ("upload", Some(subm)) => do_upload(subm),
+        _ => {},
+    }
 }
