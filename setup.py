08aeafd6c5b543a2dbc242cc6c473ae487f97ba5
Vladimir Ermakov <vooon341@gmail.com>
2016-02-09 22:54:15 +0300

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fd:
 
 setup(
     name='ota-mkfw',
-    version='0.0.1',
+    version='0.1.0',
     description='Simple firmware image packer',
     url='https://github.com/vooon/ota-mkfw',
 
